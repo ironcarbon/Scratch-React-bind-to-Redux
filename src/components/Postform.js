e312e30d5cead1cf814690fc3609bb48bdbd00ca
ipek <ipekcivicakan@gmail.com>
2018-12-07 16:58:54 -0800

    added form with controlled element

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -2,11 +2,37 @@ import React, { Component } from 'react';
 
 export default class extends Component {
 
+    state = {
+        title: "",
+        body: "",
+    }
+
+
+    onSubmitHandler = (e) => {
+
+        e.preventDefault();
+    }
+
+
     render() {
         return (
             <div>
-                Postform
+                <h1>Add Post</h1>
+                <form onSubmit={this.onSubmitHandler}>
+                    <div>
+                        <label>Title:</label><br />
+                        <input type="text" name="title" value={this.state.title} onChange={e => this.setState({ title: e.target.value })}></input>
+                    </div>
+                    <div>
+                        <label>Body:</label><br />
+                        <textarea name="body" value={this.state.body} onChange={e => this.setState({ body: e.target.value })} />
+                    </div>
+                    <div>
+                        <button type="submit">Submit</button>
+                    </div>
+                </form>
             </div>
         )
     }
 }
+
